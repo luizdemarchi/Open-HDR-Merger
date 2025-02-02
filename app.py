64011b3147d237538005fe1e725bfb8777fa2c2a
luizdemarchi <luizlham@gmail.com>
2025-02-02 20:35:51 -0300

    Adding my name

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def merge_hdr(images):
 # Streamlit UI
 st.title("Open HDR Merger 🌄")
 st.write("Upload 3-5 bracketed exposure images (same resolution)")
-st.write("Tool by: https://github.com/luizdemarchi")
+st.write("Tool by: Luiz de Marchi - https://github.com/luizdemarchi")
 
 uploaded_files = st.file_uploader(
     "Choose images (JPG/PNG/TIFF)",
